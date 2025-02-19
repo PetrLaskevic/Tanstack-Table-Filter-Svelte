e33338fb6e8bbca1923508b7962c29c1fef3c5d7
Your Name <you@example.com>
2025-02-19 23:01:39 +0100

    Remove data-table.actions (ellipsis icon menu button column altogether

--- src/routes/columns.ts
+++ src/routes/columns.ts
@@ -46,16 +46,6 @@ export const columns: ColumnDef<User>[] = [
     },
 
 
-    //A column for the Ellipsis buttons which can act as a menu
-    // => I think selecting rows with checkbox and right click context menu is better
-    // {
-    //     id: "actions",
-    //     cell: ({ row }) => {
-    //     // You can pass whatever you need from `row.original` to the component
-    //     return renderComponent(DataTableActions, { id: row.original.id });
-    //     },
-    // },
-
     //A new column for hosting the checkbox
     {
         id: "select",
