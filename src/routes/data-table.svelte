af4ec7ebbb4a6b172fbf45c81eca209bb37c30ee
Your Name <you@example.com>
2025-02-19 15:52:22 +0100

    Add filtering

--- src/routes/data-table.svelte
+++ src/routes/data-table.svelte
@@ -1,11 +1,12 @@
 <script lang="ts" generics="TData, TValue">
-import { type ColumnDef, getCoreRowModel, type PaginationState, type SortingState, getPaginationRowModel, getSortedRowModel } from "@tanstack/table-core";
+import { type ColumnDef, getCoreRowModel, type PaginationState, type SortingState, type ColumnFiltersState, getPaginationRowModel, getSortedRowModel, getFilteredRowModel } from "@tanstack/table-core";
 import { 
     createSvelteTable,
     FlexRender,
 } from "$lib/components/ui/data-table/index.js";
 import * as Table from "$lib/components/ui/table/index.js";
 import { Button } from "$lib/components/ui/button/index.js";
+import Input from "$lib/components/ui/input/input.svelte";
  
 type DataTableProps<TData, TValue> = {
     columns: ColumnDef<TData, TValue>[];
@@ -16,6 +17,7 @@ let { data, columns }: DataTableProps<TData, TValue> = $props();
 
 let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
 let sorting = $state<SortingState>([]);
+let columnFilters = $state<ColumnFiltersState>([]);
  
 const table = createSvelteTable({
     get data() {
@@ -28,6 +30,9 @@ const table = createSvelteTable({
         },
         get sorting() {
             return sorting;
+        },
+        get columnFilters() {
+            return columnFilters;
         },
     },
     onSortingChange: (updater) => {
@@ -44,68 +49,91 @@ const table = createSvelteTable({
             pagination = updater;
         }
     },
+    onColumnFiltersChange: (updater) => {
+      if (typeof updater === "function") {
+        columnFilters = updater(columnFilters);
+      } else {
+        columnFilters = updater;
+      }
+    },
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
 });
 </script>
- 
-<div class="rounded-md border">
- <Table.Root>
-    <Table.Header>
-     {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-        <Table.Row>
-         {#each headerGroup.headers as header (header.id)}
-            <Table.Head>
-             {#if !header.isPlaceholder}
-                <FlexRender
-                 content={header.column.columnDef.header}
-                 context={header.getContext()}
-                />
-             {/if}
-            </Table.Head>
-         {/each}
-        </Table.Row>
-     {/each}
-    </Table.Header>
-    <Table.Body>
-     {#each table.getRowModel().rows as row (row.id)}
-        <Table.Row data-state={row.getIsSelected() && "selected"}>
-         {#each row.getVisibleCells() as cell (cell.id)}
-            <Table.Cell>
-             <FlexRender
-                content={cell.column.columnDef.cell}
-                context={cell.getContext()}
-             />
-            </Table.Cell>
-         {/each}
-        </Table.Row>
-     {:else}
-        <Table.Row>
-         <Table.Cell colspan={columns.length} class="h-24 text-center">
-            No results.
-         </Table.Cell>
-        </Table.Row>
-     {/each}
-    </Table.Body>
- </Table.Root>
-</div>
 
-<div class="flex items-center justify-end space-x-2 py-4">
-    <Button
-      variant="outline"
-      size="sm"
-      onclick={() => table.previousPage()}
-      disabled={!table.getCanPreviousPage()}
-    >
-      Previous
-    </Button>
-    <Button
-      variant="outline"
-      size="sm"
-      onclick={() => table.nextPage()}
-      disabled={!table.getCanNextPage()}
-    >
-      Next
-    </Button>
-  </div>
+<div>
+    <div class="flex items-center py-4">
+        <Input
+        placeholder="Filter emails..."
+        value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+        onchange={(e) => {
+            table.getColumn("email")?.setFilterValue(e.currentTarget.value);
+        }}
+        oninput={(e) => {
+            table.getColumn("email")?.setFilterValue(e.currentTarget.value);
+        }}
+        class="max-w-sm"
+        />
+    </div>
+    <div class="rounded-md border">
+    <Table.Root>
+        <Table.Header>
+        {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+            <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+                <Table.Head>
+                {#if !header.isPlaceholder}
+                    <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                    />
+                {/if}
+                </Table.Head>
+            {/each}
+            </Table.Row>
+        {/each}
+        </Table.Header>
+        <Table.Body>
+        {#each table.getRowModel().rows as row (row.id)}
+            <Table.Row data-state={row.getIsSelected() && "selected"}>
+            {#each row.getVisibleCells() as cell (cell.id)}
+                <Table.Cell>
+                <FlexRender
+                    content={cell.column.columnDef.cell}
+                    context={cell.getContext()}
+                />
+                </Table.Cell>
+            {/each}
+            </Table.Row>
+        {:else}
+            <Table.Row>
+            <Table.Cell colspan={columns.length} class="h-24 text-center">
+                No results.
+            </Table.Cell>
+            </Table.Row>
+        {/each}
+        </Table.Body>
+    </Table.Root>
+    </div>
+
+    <div class="flex items-center justify-end space-x-2 py-4">
+        <Button
+        variant="outline"
+        size="sm"
+        onclick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+        >
+        Previous
+        </Button>
+        <Button
+        variant="outline"
+        size="sm"
+        onclick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+        >
+        Next
+        </Button>
+    </div>
+</div>
